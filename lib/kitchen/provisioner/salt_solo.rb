9540855824908ab761896879e1519d199402694c
Daniel Wozniak <dan@woz.io>
2018-03-04 14:07:42 -0700

    Copy config dir

--- lib/kitchen/provisioner/salt_solo.rb
+++ lib/kitchen/provisioner/salt_solo.rb
@@ -231,6 +231,9 @@ module Kitchen
           salt_call = "c:\\salt\\salt-call.bat"
           salt_config_path = config[:salt_config].tr('/', '\\')
           cmd << "(get-content #{File.join(config[:root_path], salt_config_path, 'minion').tr('/', '\\')}) -replace '\\$env:TEMP', $env:TEMP | set-content #{File.join(config[:root_path], salt_config_path, 'minion').tr('/', '\\')} ;"
+          cmd << "mkdir #{File.join(config[:root_path], 'conf').tr('/', '\\')} ;"
+          cmd << "copy #{File.join(config[:root_path], salt_config_path).tr('/', '\\')}\\* #{File.join(config[:root_path], 'conf').tr('/', '\\')}\\ ;"
+
         else
           # install/update dependencies
           cmd << sudo("chmod +x #{config[:root_path]}/*.sh;")
