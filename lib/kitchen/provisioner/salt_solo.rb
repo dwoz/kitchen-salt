22c5fae96385ee8ed553975f8800fa195262eba3
Daniel Wozniak <dan@woz.io>
2018-03-06 11:33:40 -0700

    Fix config path

--- lib/kitchen/provisioner/salt_solo.rb
+++ lib/kitchen/provisioner/salt_solo.rb
@@ -233,17 +233,18 @@ module Kitchen
           salt_config_path = config[:salt_config].tr('/', '\\')
           cmd << "(get-content #{File.join(config[:root_path], salt_config_path, 'minion').tr('/', '\\')}) -replace '\\$env:TEMP', $env:TEMP | set-content #{File.join(config[:root_path], salt_config_path, 'minion').tr('/', '\\')} ;"
           # TODO: This is to hackey, maybe we can get the config dir to be kitchen\conf earlier on someplace.
-          cmd << "mkdir #{File.join(config[:root_path], 'conf').tr('/', '\\')} ;"
-          cmd << "copy #{File.join(config[:root_path], salt_config_path).tr('/', '\\')}\\* #{File.join(config[:root_path], 'conf').tr('/', '\\')}\\ ;"
-          cmd << "$env:PATH=\"C:\\salt\\bin;$env:PATH\" ;"
+          #cmd << "mkdir #{File.join(config[:root_path], 'conf').tr('/', '\\')} ;"
+          #cmd << "copy #{File.join(config[:root_path], salt_config_path).tr('/', '\\')}\\* #{File.join(config[:root_path], 'conf').tr('/', '\\')}\\ ;"
+          #cmd << "$env:PATH=\"C:\\salt\\bin;$env:PATH\" ;"
+          cmd << sudo("#{salt_call} --state-output=changes --config-dir=#{File.join(config[:root_path], salt_config_path).tr('/', '\\')} --local state.highstate")
         else
           # install/update dependencies
           cmd << sudo("chmod +x #{config[:root_path]}/*.sh;")
           cmd << sudo("#{config[:root_path]}/dependencies.sh;")
           salt_config_path = config[:salt_config]
           salt_call = 'salt-call'
+          cmd << sudo("#{salt_call} --state-output=changes --config-dir=#{File.join(config[:root_path], salt_config_path)} --local state.highstate")
         end
-        cmd << sudo("#{salt_call} --state-output=changes --config-dir=#{File.join(config[:root_path], salt_config_path)} --local state.highstate")
         cmd << " --log-level=#{config[:log_level]}" if config[:log_level]
         cmd << " --id=#{config[:salt_minion_id]}" if config[:salt_minion_id]
         cmd << " test=#{config[:dry_run]}" if config[:dry_run]
